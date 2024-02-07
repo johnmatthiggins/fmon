7a0b6a725e19b30822772c8ad9673fd960ed4fdf
John Higgins <johnmatthiggins@gmail.com>
2024-02-07 13:39:19 -0800

    initial commit

--- fmon.go
+++ fmon.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+func handleFile(path string, entry fs.DirEntry, err error) error {
+	if !entry.IsDir() {
+		fmt.Printf("Visited: %s\n", path)
+	}
+	return nil
+}
+
+func deleteEmpty(s []string) []string {
+	var r []string
+	for _, str := range s {
+		if str != "" {
+			r = append(r, str)
+		}
+	}
+	return r
+}
 
 func main() {
-	fmt.Println("vim-go")
+	ignore := buildIgnoreExpression(".gitignore")
+	fmt.Println(ignore)
+	filepath.WalkDir(".", func(path string, entry fs.DirEntry, err error) error {
+		matched, err := regexp.MatchString(ignore, path)
+		if err == nil && !matched && !entry.IsDir() {
+			fmt.Printf("visited path at: %s\n", path)
+		} else if err != nil {
+			fmt.Println(err)
+		}
+		return nil
+	})
+}
+
+func buildIgnoreExpression(path string) string {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		panic("oh no!")
+	}
+
+	text := string(data)
+	expressions := strings.Split(text, "\n")
+
+	for i, value := range expressions {
+		expressions[i] = strings.Replace(value, ".", "\\.", -1)
+	}
+	var ignoreExpression string = strings.Join(deleteEmpty(expressions), "|")
+
+	return ignoreExpression
 }
